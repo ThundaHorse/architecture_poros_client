7439a0d9f146df3db7ed860ff56c6b6650742344
ThundaHorse <abeeeeekim@gmail.com>
2019-05-28 19:50:17 -0500

    update and delete added

--- app/controllers/client/buildings_controller.rb
+++ app/controllers/client/buildings_controller.rb
@@ -26,37 +26,27 @@ class Client::BuildingsController < ApplicationController
     render 'show.html.erb'
   end
 
-
-  # def edit
-  #   response = HTTP.get("http://localhost:3000/api/buildings/#{params[:id]}")
-  #   @building = response.parse
-  #   render "edit.html.erb"
-  # end
-
   def edit
     @building = Building.find(params[:id])
     render "edit.html.erb"
   end
 
   def update
-    client_params = {
+    @building = Building.find(params[:id])
+    @building.update(
                      name: params[:name],
                      address: params[:address],
                      height: params[:height],
                      construction_date: params[:construction_date],
                      architect: params[:architect]
-                    }
+                    )
 
-    response = HTTP.patch(
-                          "http://localhost:3000/api/buildings/#{params[:id]}", 
-                          form: client_params
-                          )
-
-    redirect_to "/client/buildings/#{params[:id]}"
+    redirect_to "/client/buildings/#{@building.id}"
   end
 
   def destroy
-    response = HTTP.delete("http://localhost:3000/api/buildings/#{params[:id]}")
+    @building = Building.find(params[:id])
+    @building.destroy
     redirect_to "/"
   end
 end
--- app/models/building.rb
+++ app/models/building.rb
@@ -2,17 +2,17 @@ class Building
   attr_accessor :name, :address, :height, :construction_date, :architect, :id
 
   def initialize(input_options)
-    @name = input_options["name"]
-    @address  = input_options["address"]
-    @height = input_options["height"]
-    @id = input_options["id"]
-    @construction_date = input_options["construction_date"]
-    @architect = input_options["architect"] 
+    @name = input_options['name']
+    @address  = input_options['address']
+    @height = input_options['height']
+    @id = input_options['id']
+    @construction_date = input_options['construction_date']
+    @architect = input_options['architect']
   end
 
   def self.find(input_id) 
     response = HTTP.get("http://localhost:3000/api/buildings/#{input_id}")
-    Building.new(response.parse) 
+    Building.new(response.parse)
   end 
 
   def self.all
@@ -25,10 +25,22 @@ class Building
     Building.new(response.parse)
   end 
 
-  def self.edit(input_id)
-    response = HTTP.get("http://localhost:3000/api/buildings/#{input_id}")
-    # response
-    Building.new()
+  def update(input_options)
+    response = HTTP.patch(
+                          "http://localhost:3000/api/buildings/#{@id}",
+                          form: input_options
+                          ).parse
+    @id = response['id']
+    @name = response['name']
+    @address = response['address']
+    @height = response['height']
+    @construction_date = response['construction_date']
+    @architect = response['architect']
+    @image = response['image']
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/buildings/#{@id}").parse 
   end 
 
 end
